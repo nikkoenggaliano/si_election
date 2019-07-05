ce3d6b859da3cc30a29920f5979636fd4dcb2427
Achmad Ainun GR <48826678+achmadainungr@users.noreply.github.com>
2019-07-05 19:45:27 +0700

    Update vote.php

--- main/vote.php
+++ main/vote.php
@@ -18,6 +18,31 @@ include 'authload.php';
 	
 
 ?>
+<?php
+$vote = $_REQUEST['vote'];
+
+
+$filename = "poll_result.txt";
+$content = file($filename);
+
+
+$array = explode("||", $content[0]);
+$yes = $array[0];
+$no = $array[1];
+
+if ($vote == 0) {
+  $yes = $yes + 1;
+}
+if ($vote == 1) {
+  $no = $no + 1;
+}
+
+
+$insertvote = $yes."||".$no;
+$fp = fopen($filename,"w");
+fputs($fp,$insertvote);
+fclose($fp);
+?>
 
 
 <!DOCTYPE html>
